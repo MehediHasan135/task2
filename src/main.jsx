71ee92b90f56bb3c44e2e18624c523d2fa6c464f
Mehedi Hasan <mehedihasan126708@gmail.com>
2025-12-04 12:02:57 +0600

    Restore react-router-dom and install package

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,15 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import WeatherPage from "./WeatherPage.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <App />
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/weather" element={<WeatherPage />} />
+    </Routes>
+  </BrowserRouter>
 );
 
